7fe599c3ddf16178d56ad89645651e5566fe11d2
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-23 18:36:58 -0500

    propane images

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->varchar('email')->unique(45);
             $table->string('password', 60);
+            $table->bool('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
