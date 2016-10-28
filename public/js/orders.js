b69d8e725884a0a5a571546bcc78e27954496ed3
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-28 18:15:43 -0500

    Created JS File for Orders pages

---
+++ public/js/orders.js
@@ -0,0 +1,14 @@
+console.log();
+
+$("#charcoalButton").click(function(){
+    // var charcoalValue = $(this).attr("value");
+    var charcoal = "2";
+    charcoal = $("qta_field_fuel").val();
+    // console.log(charcoalValue);
+})
+
+$("#propaneButton").click(function(){
+    var propaneValue = $(this).attr("value");
+    propaneValue = $("qta_field_fuel").val;
+    console.log();
+})
