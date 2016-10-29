1c5108bd8348e529d04c0aaaee7272bdb79b1108
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-28 19:10:33 -0500

    Buy and Rent selections now filter correctly and send to Items page to custom view

--- resources/views/createOrderRent.blade.php
+++ resources/views/createOrderRent.blade.php
@@ -6,9 +6,9 @@
 
         {{-- Sample Test Form --}}
             <form action="{{ action('ItemController@index') }}" method="GET" id="itemsform">
-                <input type="" name="fuel">
-                <input type="" name="size">
-                <button type="submit" class="btn btn-primary">Select</button>
+                <input type="hidden" id="qta_field_fuel" name="fuel" value="">
+                <input type="hidden" id="qta_field_size" name="size" value="">
+                {{-- <button type="submit" class="btn btn-primary">Select</button> --}}
             </form>
         {{-- End Sample Test Form --}}
 
@@ -52,7 +52,7 @@
         <div class="row calenderDiv" id="calenderDiv">
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 calender selectingADay">
                 <div class="btn btn-default dateButton" id="selectingADayButton">
-                    <h4 class="sizeButtonText smallSizeButtonText">Selecting A Day</h4>
+                    <h4 class="sizeButtonText smallSizeButtonText">Select A Day</h4>
                 </div>
                 <div class="dateInputDiv selectingADayDiv">
                     <input type="text" name="selectingADay" value="" class="dateInput" id="selectingADay"/>
@@ -72,12 +72,12 @@
                 <h1>What Type Of Grill Are You Looking For?</h1>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 charcoalDiv">
-                <div class="btn btn-default charcoalButton" id="charcoalButton">
+                <div class="btn btn-default charcoalButton" id="charcoalButton" value="2">
                     <h4 class="charcoalText">Charcoal</h4>
                 </div>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 propaneDiv">
-                <div class="btn btn-default propaneButton" id="propaneButton">
+                <div class="btn btn-default propaneButton" id="propaneButton" value="1">
                     <h4 class="propaneText">Propane</h4>
                 </div>
             </div>
@@ -87,34 +87,38 @@
                 <h1>What Size Grill Are You Looking For?</h1>
             </div>
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <a href="/items">
-                    <div class="btn btn-default sizeButton smallSizeButton" id="smallSizeButton">
+                <a>
+                    <div class="btn btn-default sizeButton smallSizeButton" id="smallSizeButton" value="Small">
                         <h4 class="sizeButtonText smallSizeButtonText">Small</h4>
                     </div>
                 </a>
             </div>
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <a href="/items">
-                    <div class="btn btn-default sizeButton mediumSizeButton" id="mediumSizeButton">
+                <a>
+                    <div class="btn btn-default sizeButton mediumSizeButton" id="mediumSizeButton" value="Medium">
                         <h4 class="sizeButtonText mediumSizeButtonText">Medium</h4>
                     </div>
                 </a>
             </div>
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <a href="/items">
-                    <div class="btn btn-default sizeButton largeSizeButton" id="largeSizeButton">
+                <a>
+                    <div class="btn btn-default sizeButton largeSizeButton" id="largeSizeButton" value="Large">
                         <h4 class="sizeButtonText largeSizeButtonText">Large</h4>
                     </div>
                 </a>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <a href="/items">
-                    <div class="btn btn-default imNotSureButton" id="imNotSureButton">
+                <a>
+                    <div class="btn btn-default imNotSureButton" id="imNotSureButton" value="%">
                         <h4 class="imNotSureButtonText">I'm Not Sure</h4>
                     </div>
                 </a>
             </div>
         </div>
     </div>
+
+     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+                <script src="/js/orders.js"></script>
+
 
 @stop
