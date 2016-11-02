c8fef193383a7506eef3579174f8d4920344d0af
Rachel Rost <rachel.v.rost@gmail.com>
2016-11-01 19:31:59 -0500

    Added Favicon

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 <link href="/css/main.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="/getgrillz_images/favicon.ico"/>
