bcfaf1d4140c3e4d708822bcab6737de3d3a2237
Wyatt Barnes <wyattb413@gmail.com>
2016-11-02 11:20:06 -0500

    added cheveron and automatic scrolling to pickADate section on create order page

--- public/js/createOrder.js
+++ public/js/createOrder.js
@@ -44,31 +44,64 @@ $("#propaneButton").click(function(){
     $("#charcoalButton").removeClass("active");
 });
 
+// TOGGLES THE ACTIVE CLASS BETWEEN CHOOSE A SPECIFIC DAY AND JUST BROWSING
+$("#selectingADayButton").click(function(){
+    $('.chevronCalender1').slideToggle()
+    if ($('.chevronCalender2').is(":visible")) {
+        $('.chevronCalender2').slideToggle()
+    }
+    if (!$('.whatTypeOfGrillDiv').is(":visible")) {
+        $('.whatTypeOfGrillDiv').slideToggle()
+    }
+});
+
+$("#selectingMultipleButton").click(function(){
+    $('.chevronCalender2').slideToggle()
+    if ($('.chevronCalender1').is(":visible")) {
+        $('.chevronCalender1').slideToggle()
+    }
+    if (!$('.whatTypeOfGrillDiv').is(":visible")) {
+        $('.whatTypeOfGrillDiv').slideToggle()
+    }
+});
+
+$("#chevronCalender1").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".whatTypeOfGrillDiv").offset().top
+    }, 1000);
+});
+
+$("#chevronCalender2").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".whatTypeOfGrillDiv").offset().top
+    }, 1000);
+});
+
 // TOGGLES THE ACTIVE CLASS BETWEEN SMALL MEDIUM AND LARGE BUTTONS
-$("#smallSizeButton").click(function(){
-    $("#smallSizeButton").addClass("active");
-    $("#mediumSizeButton").removeClass("active");
-    $("#largeSizeButton").removeClass("active");
-    $("#imNotSureButton").removeClass("active");
-});
-
-$("#mediumSizeButton").click(function(){
-    $("#mediumSizeButton").addClass("active");
-    $("#smallSizeButton").removeClass("active");
-    $("#largeSizeButton").removeClass("active");
-    $("#imNotSureButton").removeClass("active");
-});
-
-$("#largeSizeButton").click(function(){
-    $("#largeSizeButton").addClass("active");
-    $("#smallSizeButton").removeClass("active");
-    $("#mediumSizeButton").removeClass("active");
-    $("#imNotSureButton").removeClass("active");
-});
-
-$("#imNotSureButton").click(function(){
-    $("#imNotSureButton").addClass("active");
-    $("#smallSizeButton").removeClass("active");
-    $("#largeSizeButton").removeClass("active");
-    $("#mediumSizeButton").removeClass("active");
-});
+// $("#smallSizeButton").click(function(){
+//     $("#smallSizeButton").addClass("active");
+//     $("#mediumSizeButton").removeClass("active");
+//     $("#largeSizeButton").removeClass("active");
+//     $("#imNotSureButton").removeClass("active");
+// });
+//
+// $("#mediumSizeButton").click(function(){
+//     $("#mediumSizeButton").addClass("active");
+//     $("#smallSizeButton").removeClass("active");
+//     $("#largeSizeButton").removeClass("active");
+//     $("#imNotSureButton").removeClass("active");
+// });
+//
+// $("#largeSizeButton").click(function(){
+//     $("#largeSizeButton").addClass("active");
+//     $("#smallSizeButton").removeClass("active");
+//     $("#mediumSizeButton").removeClass("active");
+//     $("#imNotSureButton").removeClass("active");
+// });
+//
+// $("#imNotSureButton").click(function(){
+//     $("#imNotSureButton").addClass("active");
+//     $("#smallSizeButton").removeClass("active");
+//     $("#largeSizeButton").removeClass("active");
+//     $("#mediumSizeButton").removeClass("active");
+// });
