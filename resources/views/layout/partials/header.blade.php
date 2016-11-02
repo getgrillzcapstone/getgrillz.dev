0e91e6fd0ec8acbdf7499e2d993f54627c7d5ad1
Wyatt Barnes <wyattb413@gmail.com>
2016-11-02 10:51:35 -0500

    Redesigned create order page, as well as made the view responsive and added slide toggles

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,4 +1,5 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
-<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+{{-- <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"> --}}
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link href="/css/main.css" rel="stylesheet" type="text/css" />
